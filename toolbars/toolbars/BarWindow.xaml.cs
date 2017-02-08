673bd25f2197ec8f9aa4706bae9a9371cd960825
Chris Huys <chrishuys@live.com>
2017-02-08 13:57:40 +0100

    voorhotkeys

--- toolbars/toolbars/BarWindow.xaml.cs
+++ toolbars/toolbars/BarWindow.xaml.cs
@@ -20,12 +20,30 @@ namespace toolbars
     /// </summary>
     public partial class BarWindow : Window
     {
+        public static RoutedCommand mijnRoutectrlb = new RoutedCommand();
+        public static RoutedCommand mijnRoutectrli = new RoutedCommand();
+
         public BarWindow()
         {
             InitializeComponent();
+
+            CommandBinding mijnctrlb = new CommandBinding(mijnRoutectrlb, ctrlbExecuted);
+            this.CommandBindings.Add(mijnctrlb);
+
+            KeyGesture toetsctrlb = new KeyGesture(Key.B, ModifierKeys.Control);
+            KeyBinding mijnKeyCtrlB = new KeyBinding(mijnRoutectrlb, toetsctrlb);
+            this.InputBindings.Add(mijnKeyCtrlB);
+
+            CommandBinding mijnctrli = new CommandBinding(mijnRoutectrli, ctrliExecuted);
+            this.CommandBindings.Add(mijnctrli);
+
+            KeyGesture toetsctrli = new KeyGesture(Key.I, ModifierKeys.Control);
+            KeyBinding mijnKeyCtrlI = new KeyBinding(mijnRoutectrli, toetsctrli);
+            this.InputBindings.Add(mijnKeyCtrlI);
+
         }
 
-
+        
 
         private void Vet_Aan_Uit()
         {
@@ -75,6 +93,16 @@ namespace toolbars
             hetlettertype.IsChecked = true;
             voorbeeldtekst.FontFamily = new FontFamily(hetlettertype.Header.ToString());
            
+        }
+
+        private void ctrlbExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            Vet_Aan_Uit();
+        }
+
+        private void ctrliExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            Schuin_Aan_Uit();
         }
     }
 }
